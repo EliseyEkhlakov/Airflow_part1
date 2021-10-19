51dc5d20d8692fc6ca2ec665b533360ccc47b580
Елисей Ехлаков <elisey.ekhlakov@krit.pro>
2021-10-19 18:42:00 +0400

    +subdag

--- parallel_dag.py
+++ parallel_dag.py
@@ -1,13 +1,15 @@
 from airflow import DAG
 from datetime import datetime
 from airflow.operators.bash import BashOperator
+from airflow.operators.subdag import SubDagOperator
+from subdags.subdag_parallel_dag import subdag_parallel_dag
 
 default_args = {
     'start_date': datetime(2020,1,1)
 }
 
 with DAG('parallel_dag', schedule_interval='@daily',
-        default_args=default_args,
+        default_args=default_args,        
         catchup=False) as dag:
 
         task_1 = BashOperator(
@@ -15,19 +17,15 @@ with DAG('parallel_dag', schedule_interval='@daily',
             bash_command='sleep 3'
         )
 
-        task_2 = BashOperator(
-            task_id='task_2',
-            bash_command='sleep 3'
+        processing = SubDagOperator(
+            task_id='processing_tasks',
+            subdag=subdag_parallel_dag('parallel_dag', 'processing_tasks', default_args)
         )
-
-        task_3 = BashOperator(
-            task_id='task_3',
-            bash_command='sleep 3'
-        )
+        
 
         task_4 = BashOperator(
             task_id='task_4',
             bash_command='sleep 3'
         )        
 
-        task_1 >> [task_2, task_3] >> task_4
+        task_1 >> processing >> task_4
